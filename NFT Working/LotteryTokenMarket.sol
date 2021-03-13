5c989f02c9c9ae836b05295fd7cc55c100f2fcbe
Kevin Walsh <kevinjwalsh1997@gmail.com>
2021-03-13 14:45:52 -0500

    Updated

--- NFT Working/LotteryTokenMarket.sol
+++ NFT Working/LotteryTokenMarket.sol
@@ -17,6 +17,14 @@ contract LotteryTokenMarketPlace is Ownable {
     uint[] public lotterytokens;
     // Address of the lottery runner (i.e. the house)
     address payable The_House;
+    // Structure containing the participants information
+    struct Participants {
+        string name;
+        address purchaser;
+    }
+    
+    // Mapping Token id to structure above
+    mapping(uint256 => Participants) public participant_information;
     // Mapping of token id to the address of the purchaser
     mapping(uint256 => address payable) public tokendict;
     // The token being sold
@@ -24,7 +32,7 @@ contract LotteryTokenMarketPlace is Ownable {
     // Address where funds are collected --> in this case the contract
     address payable addr = address(uint160(address(this)));
     // Address of purchaser
-    address payable purchaser;
+    //address payable purchaser;
     // Setting the lottery state as live through a boolean
     bool public isLotteryLive;
     // The price for a token
@@ -54,7 +62,7 @@ contract LotteryTokenMarketPlace is Ownable {
         isLotteryLive = false;
     }
     // Function for buying tokens, which will serve as the lottery tickets. The lottery must be live.
-    function buyToken(address payable purchaser) external payable {
+    function buyToken(string calldata name, address payable purchaser) external payable {
         require(isLotteryLive == true, "Lottery is not currently running");
         require(msg.value >= _price,"Not enough funds");
         uint256 ETHAmount = msg.value;
@@ -69,6 +77,7 @@ contract LotteryTokenMarketPlace is Ownable {
         lotteryBag.push(purchaser);
         lotterytokens.push(lastTokenId);
         tokendict[lastTokenId] = purchaser;
+        participant_information[lastTokenId] = Participants(name, purchaser);
 
     }
     // Function for declaring the winner. Lottery cannot be live.
@@ -79,8 +88,8 @@ contract LotteryTokenMarketPlace is Ownable {
         uint index = generateRandomNumber() % lotterytokens.length;
         //lotteryBag[index].transfer(address(this).balance);
         tokendict[index].transfer(address(this).balance);
-        
     }
+    
     
     // Return the token name.
     function token() external view returns (LotteryToken) {
